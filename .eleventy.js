b030b365854325bf90a162a1378fd8c06694c9aa
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-12-19 19:57:47 -1000

    feat: add additional netlify build plugins

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,7 @@
-// Transforms
-const htmlMinTransform = require("./src/transforms/html-min.js");
-
-const isProduction = process.env.NODE_ENV === "production";
-
 module.exports = (config) => {
   config.addPassthroughCopy("./src/fonts");
   config.addPassthroughCopy("./src/img");
   config.addWatchTarget("./src/js/");
-
-  if (isProduction) {
-    config.addTransform("htmlmin", htmlMinTransform);
-  }
 
   return {
     dir: {
