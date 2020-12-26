cf37621010ec925a9ee3ad971dd41b6362dc678c
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2020-12-26 02:02:34 -1000

    feat: adds prismic deps, config and query for homepage

---
+++ src/_data/home.js
@@ -0,0 +1,8 @@
+const { client } = require('../../prismic-configuration');
+
+module.exports = async () => {
+  const home = await client.getByUID('page', 'home');
+
+  //   Even if it's one page still return in an array for 11ty pagination
+  return [home];
+};
