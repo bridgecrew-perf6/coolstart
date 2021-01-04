7b064114a9cec12e48adeb32d34feaa36ae79913
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-01-03 23:09:01 -1000

    edit: uncomments homepage

--- src/_data/home-example.js
+++ src/_data/home-example.js
@@ -1,10 +1,10 @@
 /* This is an example query for home */
 
-// const { client } = require('../../prismic');
+const { client } = require('../../prismic');
 
-// module.exports = async () => {
-//   const home = await client.getByUID('page', 'home');
+module.exports = async () => {
+  const home = await client.getByUID('page', 'home');
 
-//   // If there are multiple pages return an array
-//   return home;
-// };
+  // If there are multiple pages return an array
+  return home;
+};
