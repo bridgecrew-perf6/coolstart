e1588541b6b8baa941892575cfcc1733b94eb5d7
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-01-03 23:04:53 -1000

    feat: adds prismic configuration file and home-example file

---
+++ src/_data/home-example.js
@@ -0,0 +1,10 @@
+/* This is an example query for home */
+
+// const { client } = require('../../prismic');
+
+// module.exports = async () => {
+//   const home = await client.getByUID('page', 'home');
+
+//   // If there are multiple pages return an array
+//   return home;
+// };
