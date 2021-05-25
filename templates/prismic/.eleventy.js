78f79a759c1eb2ee0d19590695a3c31105b2354d
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-05-25 13:50:13 -1000

    feat: adds link resolver and html serializer

---
+++ templates/prismic/.eleventy.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const imageShortcode = require('./transforms/shortcodes/imageShortcode');
+const handleRichText = require('./transforms/shortcodes/handleRichText');
+const {
+	handleLinkResolver,
+} = require('./transforms/shortcodes/handleLinkResolver');
+
+module.exports = (config) => {
+	const pug = require('pug');
+	config.setLibrary('pug', pug);
+
+	config.setTemplateFormats([
+		// Templates:
+		'html',
+		'pug',
+		'md',
+		// Static Assets:
+		'css',
+		'jpeg',
+		'jpg',
+		'png',
+		'svg',
+		'woff',
+		'woff2',
+	]);
+
+	config.addPassthroughCopy('assets');
+
+	// 11ty Shortcodes
+	config.addJavaScriptFunction('image', imageShortcode);
+	config.addJavaScriptFunction('richText', handleRichText);
+	config.addJavaScriptFunction('link', handleLinkResolver);
+	config.addJavaScriptFunction('log', (val) => console.log(val));
+
+	global.filters = config.javascriptFunctions;
+	config.setPugOptions({
+		globals: ['filters'],
+	});
+
+	// Watch all 11ty specific directories
+	config.addWatchTarget(path.join(__dirname, 'img'));
+	config.addWatchTarget(path.join(__dirname, 'scss'));
+	config.addWatchTarget(path.join(__dirname, 'includes'));
+	config.addWatchTarget(path.join(__dirname, 'data'));
+	config.addWatchTarget(path.join(__dirname, 'js'));
+	config.addWatchTarget(path.join(__dirname, 'pages'));
+
+	return {
+		dir: {
+			input: path.join(__dirname, 'pages'),
+			// Relative to the 'pages' directory
+			includes: '../includes',
+			data: '../data',
+			output: '_site',
+		},
+	};
+};
---
+++ templates/prismic/transforms/shortcodes/imageShortcode.js
@@ -0,0 +1,33 @@
+const Image = require('@11ty/eleventy-img');
+
+function imageShortcode(src, alt, className = '', sizes = '100vw') {
+	if (alt === undefined) {
+		// You bet we throw an error on missing alt (alt="" works okay)
+		throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+	}
+
+	let options = {
+		widths: [300, 600],
+		formats: ['avif', 'jpeg'],
+		outputDir: '_site/img',
+	};
+
+	let imageAttributes = {
+		alt,
+		sizes,
+		class: className,
+		loading: 'lazy',
+		decoding: 'async',
+	};
+
+	Image(src, options);
+
+	let metadata = Image.statsSync(src, options);
+
+	// You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+	return Image.generateHTML(metadata, imageAttributes, {
+		whitespaceMode: 'inline',
+	});
+}
+
+module.exports = imageShortcode;
