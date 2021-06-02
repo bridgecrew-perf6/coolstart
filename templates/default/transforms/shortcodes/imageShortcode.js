8f852ec9fec61fc9a46ad2f463c372bf9e5b36fa
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-06-01 23:40:22 -1000

    feat: adds updated image shortcode to templates

--- templates/default/transforms/shortcodes/imageShortcode.js
+++ templates/default/transforms/shortcodes/imageShortcode.js
@@ -1,33 +1,42 @@
-const Image = require('@11ty/eleventy-img');
+const Image = require("@11ty/eleventy-img");
 
-function imageShortcode(src, alt, className = '', sizes = '100vw') {
-	if (alt === undefined) {
-		// You bet we throw an error on missing alt (alt="" works okay)
-		throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
-	}
+function imageShortcode(src, alt = "", className = "", sizes = "100vw") {
+  if (alt === undefined) {
+    // You bet we throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+  }
 
-	let options = {
-		widths: [300, 600],
-		formats: ['avif', 'jpeg'],
-		outputDir: '_site/img',
-	};
+  let options = {
+    widths: [300, 600],
+    class: "responsive_iamge",
+    formats: ["avif", "jpeg"],
+    outputDir: "_site/img",
+  };
 
-	let imageAttributes = {
-		alt,
-		sizes,
-		class: className,
-		loading: 'lazy',
-		decoding: 'async',
-	};
+  Image(src, options);
 
-	Image(src, options);
+  let metadata = Image.statsSync(src, options);
 
-	let metadata = Image.statsSync(src, options);
+  let lowsrc = metadata.jpeg[0];
 
-	// You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
-	return Image.generateHTML(metadata, imageAttributes, {
-		whitespaceMode: 'inline',
-	});
+  return `<picture class="responsive_image">
+    ${Object.values(metadata)
+      .map((imageFormat) => {
+        return `  <source type="${
+          imageFormat[0].sourceType
+        }" srcset="${imageFormat
+          .map((entry) => entry.srcset)
+          .join(", ")}" sizes="${sizes}">`;
+      })
+      .join("\n")}
+      <img
+        src="${lowsrc.url}"
+        width="${lowsrc.width}"
+        height="${lowsrc.height}"
+        alt="${alt}"
+        loading="lazy"
+        decoding="async">
+    </picture>`;
 }
 
 module.exports = imageShortcode;
